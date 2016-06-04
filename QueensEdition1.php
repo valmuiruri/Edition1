140507411a0feccf33716d99d3b212c3bf6efe59
valmuiruri <valentino.muiruri@gmail.com>
2016-06-04 11:10:37 -0400

    second commit
    has all the content, css, js, pictures, and base code

---
+++ QueensEdition1.php
@@ -0,0 +1,242 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Queens Edition</title>
+    <!-- Bootstrap Core CSS - Uses Bootswatch Flatly Theme: http://bootswatch.com/flatly/ -->
+    <link href="css/bootstrap.min.css" rel="stylesheet"> 
+
+    <!-- Custom CSS 
+    <link href="css/freelancer.css" rel="stylesheet"> -->
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css"> 
+</head>
+<body id="page-top" class="index">
+    <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#page-top">New Summer Courses</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="#portfolio">Courses</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+    <!-- Header -->
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <img class="img-responsive" src="img/profile.png" alt="">
+                    <div class="intro-text">
+                        <span class="name">Queen's University</span>
+                        <hr class="star-light">
+                        <span class="skills">New Courses offered for the 2016-2017 school year</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- Portfolio Grid Section -->
+    <section id="portfolio">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>Click The Image to Find Out More</h2>
+                    <hr class="star-primary">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-4 portfolio-item">
+                    <a href="#portfolioModal1" class="portfolio-link" data-toggle="modal">
+                        <div class="caption">
+                            <div class="caption-content">
+                                <i class="fa fa-search-plus fa-3x"></i>
+                            </div>
+                        </div>
+                        <img src="portfolio/cabin.png" class="img-responsive" alt="">
+                    </a>
+                </div>
+                <div class="col-sm-4 portfolio-item">
+                    <a href="#portfolioModal2" class="portfolio-link" data-toggle="modal">
+                        <div class="caption">
+                            <div class="caption-content">
+                                <i class="fa fa-search-plus fa-3x"></i>
+                            </div>
+                        </div>
+                        <img src="portfolio/cake.png" class="img-responsive" alt="">
+                    </a>
+                </div>
+                <div class="col-sm-4 portfolio-item">
+                    <a href="#portfolioModal3" class="portfolio-link" data-toggle="modal">
+                        <div class="caption">
+                            <div class="caption-content">
+                                <i class="fa fa-search-plus fa-3x"></i>
+                            </div>
+                        </div>
+                        <img src="portfolio/circus.png" class="img-responsive" alt="">
+                    </a>
+                </div>
+                <div class="col-sm-4 portfolio-item">
+                    <a href="#portfolioModal4" class="portfolio-link" data-toggle="modal">
+                        <div class="caption">
+                            <div class="caption-content">
+                                <i class="fa fa-search-plus fa-3x"></i>
+                            </div>
+                        </div>
+            </div>
+        </div>
+    </section>
+    <!-- Scroll to Top Button (Only visible on small and extra-small screen sizes) -->
+    <div class="scroll-top page-scroll hidden-sm hidden-xs hidden-lg hidden-md">
+        <a class="btn btn-primary" href="#page-top">
+            <i class="fa fa-chevron-up"></i>
+        </a>
+    </div>
+    <!-- Portfolio Modals -->
+    <div class="portfolio-modal modal fade" id="portfolioModal1" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-content">
+            <div class="close-modal" data-dismiss="modal">
+                <div class="lr">
+                    <div class="rl">
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-lg-offset-2">
+                        <div class="modal-body">
+                            <h2>Worlds in Place: Settler and Indigenous Stories of Kingston/Cateraqui</h2>
+                            <hr class="star-primary">
+                            <img src="portfolio/game.png" class="img-responsive img-centered" alt="">
+                            <p>An English Language and Literature seminar course that explores the Indigenous history of the land through archival materials, community conversations, and a mix of memoir, poetry and artwork. <a href="http://www.indiviapp.com/">PUT LINK TO COURSE DESCRIPTION</a>.</p>
+                            <ul class="list-inline item-details">
+                                <li>Course:
+                                    <strong><a href="http://startbootstrap.com">ENG 467</a>
+                                    </strong>
+                                </li>
+								<li>Faculty
+                                    <strong><a href="http://startbootstrap.com">Arts and Science</a>
+                                    </strong>
+                                </li>
+                                <li>Professor
+                                    <strong><a href="http://startbootstrap.com">Dr. Laura Murray</a>
+                                    </strong>
+                                </li>
+                            </ul>
+                            <button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa fa-times"></i> Close</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="portfolio-modal modal fade" id="portfolioModal2" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-content">
+            <div class="close-modal" data-dismiss="modal">
+                <div class="lr">
+                    <div class="rl">
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-lg-offset-2">
+                        <div class="modal-body">
+                            <h2>Environment and Sustainability</h2>
+                            <hr class="star-primary">
+                            <img src="portfolio/cake.png" class="img-responsive img-centered" alt="">
+                            <p>This course gives a foundation of sustainability knowledge and what is needed to reach future sustainability goals. <a href="C:\wamp64\www\typica login\index.html">Taco 'bout it</a> is the solution.</p>
+                            <ul class="list-inline item-details">
+                                <li>Course
+                                    <strong><a href="http://startbootstrap.com">ENSC 203</a>
+                                    </strong>
+                                </li>
+								<li>Faculty
+                                    <strong><a href="http://startbootstrap.com">School of Environmental Studies </a>
+                                    </strong>
+                                </li>
+                                <li>Professors
+                                    <strong><a href="http://startbootstrap.com">Dr. Allison Goebel, Dr. Stephen Brown, and Dr. Alice Hovorka </a>
+                                    </strong>
+                                </li>
+                            </ul>
+                            <button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa fa-times"></i> Close</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="portfolio-modal modal fade" id="portfolioModal3" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-content">
+            <div class="close-modal" data-dismiss="modal">
+                <div class="lr">
+                    <div class="rl">
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-lg-offset-2">
+                        <div class="modal-body">
+                            <h2>Population and Global Health</h2>
+                            <hr class="star-primary">
+                            <img src="portfolio/circus.png" class="img-responsive img-centered" alt="">
+                            <p>Introduces students to the foundation of population and global health in relation to Indigenous history and culture better preparing students for difficult cases. <a href="https://sellfy.com/p/8Q9P/jV3VZ/">Graphic of skills learned</a>. I have learned these skills</p>
+                            <ul class="list-inline item-details">
+                                <li>Course
+                                    <strong><a href="http://startbootstrap.com">MEDS 116</a>
+                                    </strong>
+                                </li>
+								<li>Faculty
+                                    <strong><a href="http://startbootstrap.com">School of Medicine</a>
+                                    </strong>
+                                </li>
+                                <li>Professors
+                                    <strong><a href="http://startbootstrap.com">Dr. Lindsay Davidson and Dr. Melanie Walker</a>
+                                    </strong>
+                                </li>
+                            </ul>
+                            <button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa fa-times"></i> Close</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script> 
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script> 
+
+    <!-- Plugin JavaScript 
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    <script src="js/classie.js"></script>
+    <script src="js/cbpAnimatedHeader.js"></script>  -->
+
+    <!-- Custom Theme JavaScript 
+    <script src="js/freelancer.js"></script> -->
+</body>
+</html>
