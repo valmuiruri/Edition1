826a5c9b8eace64f795fa20089bf017d19c6bea6
valmuiruri <valentino.muiruri@gmail.com>
2016-06-05 16:28:45 -0400

    small change to php file

--- QueensEdition1.php
+++ QueensEdition1.php
@@ -25,7 +25,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#page-top">Queen's University New Courses</a>
+                <a class="navbar-brand" href="#page-top">Queen's University</a>
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
